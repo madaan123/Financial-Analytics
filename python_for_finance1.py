43c98b0d165095fa1687534a1ed56711d6da53c3
ashish madaan <ashishmadaan6@gmail.com>
2018-06-07 01:48:26 +0530

    Add python_for_finance first file

--- python_for_finance1.py
+++ python_for_finance1.py
@@ -5,6 +5,7 @@ Created on Thu Jun  7 00:20:45 2018
 @author: DELL
 """
 
+# Important libraries to import
 import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style
@@ -12,4 +13,16 @@ import pandas as pd
 pd.core.common.is_list_like = pd.api.types.is_list_like
 import pandas_datareader.data as web
 
-import datetime as dt
+# Using ggplot design for plottting data
+style.use('ggplot')
+
+# Defining start and end timeframe
+start = dt.datetime(2001,1,1)
+end = dt.datetime(2016,12,31)
+
+# Reading data from web of the stcoks tesla and morningstar
+df = web.DataReader('TSLA','morningstar',start,end)
+# df.head prints first 5 rows of the database
+print(df.head())
+# df.tail will print last 5 rows
+print(df.tail())
